396764435cb3944da028fa86823c5476db2005a7
ricarellano <rarellanob@gmail.com>
2017-02-27 11:43:39 -0800

    associate users with location

--- db/migrate/20170227072652_add_ip_address_to_location.rb
+++
@@ -1,5 +0,0 @@
-class AddIpAddressToLocation < ActiveRecord::Migration[5.0]
-  def change
-    add_column :locations, :ip_address, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227072652) do
+ActiveRecord::Schema.define(version: 20170227034318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20170227072652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.boolean  "ip_address"
     t.index ["user_id"], name: "index_locations_on_user_id", using: :btree
   end
 
