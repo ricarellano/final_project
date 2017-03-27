5550e99d0d3338cb3a02b917af6a377439e9734c
ricarellano <rarellanob@gmail.com>
2017-03-26 22:28:11 -0700

    refactor guilty pleasure

--- app/models/location.rb
+++ app/models/location.rb
@@ -22,4 +22,17 @@ class Location < ApplicationRecord
     location.calories = 8 * 75 * time
     location.save
   end
+
+  def self.guilty (location)
+    if location.calories < 128
+      location.food = "1 Donut"
+    elsif location.calories < 128 * 2
+      location.food = "2 Donuts"
+    elsif location.calories < 128 * 3
+      location.food = "Burguer + Beer"
+    elsif location.calories < 128 * 4
+      location.food = "Burguer + Fries + Beer"
+    end
+      location.save
+  end
 end
